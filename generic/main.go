0d061d59524c2413abbcc087e6988d5ff75d7033
mostafizur-raahman <mostafizurrahman0202@gmail.com>
2025-01-18 21:14:19 +0600

    implement generic

--- generic/main.go
+++ generic/main.go
@@ -14,9 +14,41 @@ func printSomething[T any](items []T) {
 		fmt.Println(item)
 	}
 }
+
+// struct
+type Stack struct {
+	items []int
+}
+
+// use generic
+type StackG[T any] struct {
+	items []T
+}
+
 func main() {
 	nums := []int{1, 2, 3, 4}
 	names := []string{"Mostafiz", "abir", "rayhan"}
+	vals := []bool{true, false, false, true}
 	printSomething(nums)
 	printSomething(names)
+	printSomething(vals)
+
+	// struct
+	stack := Stack{
+		items: []int{1, 2, 4, 5},
+	}
+	fmt.Println(stack)
+
+	// with generic
+	stackG := StackG[int]{
+		items: []int{1, 2, 2, 4},
+	}
+
+	stackG2 := StackG[string]{
+		items: []string{"a", "b", "c"},
+	}
+
+	fmt.Println(stackG)
+	fmt.Println(stackG2)
+
 }
