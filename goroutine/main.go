4e0ff39d5694945fc00248d290c4a3185fb848b7
mostafizur-raahman <mostafizurrahman0202@gmail.com>
2025-01-20 20:59:08 +0600

    implement goroutine

--- goroutine/main.go
+++ goroutine/main.go
@@ -2,24 +2,36 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"sync"
 )
 
-func doingSameTask(i int) {
+func doingSameTask(i int, w *sync.WaitGroup) {
+	defer w.Done()
 	fmt.Println(i)
 }
 func main() {
-	// var wg sync.WaitGroup
-	for i := 0; i < 5; i++ {
-		// non blocking : running concurrently
-		// go doingSameTask(i)
+	// // var wg sync.WaitGroup
+	// for i := 0; i < 5; i++ {
+	// 	// non blocking : running concurrently
+	// 	// go doingSameTask(i)
 
-		go func(i int) {
-			fmt.Println(i)
-		}(i)
+	// 	go func(i int) {
+	// 		fmt.Println(i)
+	// 	}(i)
+	// }
+
+	// // we don't use that later we use WaitGroup
+	// time.Sleep(time.Second * 1)
+
+	// implement wait group
+
+	var wg sync.WaitGroup
+
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go doingSameTask(i, &wg)
 	}
 
-	// we don't use that later we use WaitGroup
-	time.Sleep(time.Second * 1)
+	wg.Wait()
 
 }
