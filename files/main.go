ca513f40dee3d1a1e31ea5493d8437bb20ea71c4
mostafizur-raahman <mostafizurrahman0202@gmail.com>
2025-01-22 20:29:12 +0600

    add simple open in go

--- files/main.go
+++ files/main.go
@@ -24,24 +24,34 @@ func main() {
 
 	// read file
 
-	f, err := os.Open("example.txt")
+	// f, err := os.Open("example.txt")
+
+	// if err != nil {
+	// 	fmt.Println("Error to open file")
+	// 	panic(err)
+	// }
+
+	// defer f.Close()
+
+	// buffer := make([]byte, 10)
+
+	// d, err := f.Read(buffer)
+
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// for i := 0; i < len(buffer); i++ {
+	// 	fmt.Print(d, string(buffer[i]))
+	// }
+
+	// short cut
+
+	data, err := os.ReadFile("example.txt")
 
 	if err != nil {
-		fmt.Println("Error to open file")
 		panic(err)
 	}
 
-	defer f.Close()
-
-	buffer := make([]byte, 10)
-
-	d, err := f.Read(buffer)
-
-	if err != nil {
-		panic(err)
-	}
-
-	for i := 0; i < len(buffer); i++ {
-		fmt.Print(d, string(buffer[i]))
-	}
+	fmt.Println(string(data))
 }
